a361830c9381f15a9f484414b9a2674f905b874d
micgarcia <micgarcia9185@gmail.com>
2022-08-02 16:27:31 -0700

    Refactored App.js, GeneralForm.js, GeneralInfo.js to use functions instead of class components

--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -1,116 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GeneralInfo from './GeneralInfo.js';
 
-class GeneralForm extends React.Component {
-  constructor(props) {
-    super(props);
+const GeneralForm = () => {
+  const [currentName, setCurrentName] = useState('');
+  const [currentEmail, setCurrentEmail] = useState('');
+  const [currentPhone, setCurrentPhone] = useState('');
+  const [info, setInfo] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  });
+  const [hidden, setHidden] = useState(false);
 
-    this.state = {
-      currentName: '',
-      currentEmail: '',
-      currentPhone: '',
-      info: {
-        name: '',
-        email: '',
-        phone: ''
-      },
-      hidden: false,
-    }
+  const onSubmitGeneral = (e) => {
+    e.preventDefault();
+    setInfo({
+      name: currentName,
+      email: currentEmail,
+      phone: currentPhone
+    });
+    setCurrentName('');
+    setCurrentEmail('');
+    setCurrentPhone('');
+    setHidden(true);
   }
 
-  nameChange = (e) => {
-    this.setState({
-      currentName : e.target.value
-    });
-  };
-
-  emailChange = (e) => {
-    this.setState({
-      currentEmail : e.target.value
-    });
-  };
-
-  phoneChange = (e) => {
-    this.setState({
-      currentPhone : e.target.value
-    });
-  };
-
-  onSubmitGeneral = (e) => {
-    e.preventDefault();
-    this.setState({
-      info: {
-        name: this.state.currentName,
-        email: this.state.currentEmail,
-        phone: this.state.currentPhone
-      },
-      currentName: '',
-      currentEmail: '',
-      currentPhone: '',
-      hidden: true,
-    });
-
-  };
-
-  editGeneral = (e) => {
-    this.setState({
-      hidden: false,
-      currentName: this.state.info.name,
-      currentEmail: this.state.info.email,
-      currentPhone: this.state.info.phone,
-      info: {
+  const editGeneral = (e) => {
+    return(
+      setHidden(false),
+      setCurrentName(info.name),
+      setCurrentEmail(info.email),
+      setCurrentPhone(info.phone),
+      setInfo({
         name: '',
         email: '',
         phone: ''
-      }
-    });
+      })
+    )
   };
 
-  render() {
-    const { currentName, currentEmail, currentPhone, info, hidden} = this.state;
 
-    if (hidden) {
-      return (
-        <div className="generalContHidden">
-          {<GeneralInfo info={info}/>}
-          <button onClick={this.editGeneral}>Edit</button>
-        </div>
-      )
-    } else {
-      return (
-        <div className="generalCont">
-          <div id="generalTitle">Enter General Personal Information Below:</div>
-          <form onSubmit={this.onSubmitGeneral} id="generalForm">
-            <label htmlFor="nameInput">Name:</label>
-            <input
-              type="text"
-              id="nameInput"
-              value={currentName}
-              onChange={this.nameChange}
-            />
-            <label htmlFor="emailInput">Email:</label>
-            <input
-              type="text"
-              id="emailInput"
-              value={currentEmail}
-              onChange={this.emailChange}
-            />
-            <label htmlFor="phoneInput">Phone:</label>
-            <input
-              type="text"
-              id="phoneInput"
-              value={currentPhone}
-              onChange={this.phoneChange}
-            />
-            <button type="submit">
-              Submit
-            </button>
-          </form>
-          {<GeneralInfo info={info}/>}
+  const nameChange = (e) => {
+    console.log(e.target.value);
+    setCurrentName(e.target.value);
+  };
 
-        </div>
-      )
-    }
+  const emailChange = (e) => {
+    setCurrentEmail(e.target.value);
+  };
+
+  const phoneChange = (e) => {
+    setCurrentPhone(e.target.value);
+  };
+
+
+
+  if (hidden) {
+    return (
+      <div className="generalContHidden">
+        {<GeneralInfo info={info}/>}
+        <button onClick={editGeneral}>Edit</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className="generalCont">
+        <div id="generalTitle">Enter General Personal Information Below:</div>
+        <form onSubmit={onSubmitGeneral} id="generalForm">
+          <label htmlFor="nameInput">Name:</label>
+          <input
+            type="text"
+            id="nameInput"
+            value={currentName}
+            onChange={nameChange}
+          />
+          <label htmlFor="emailInput">Email:</label>
+          <input
+            type="text"
+            id="emailInput"
+            value={currentEmail}
+            onChange={emailChange}
+          />
+          <label htmlFor="phoneInput">Phone:</label>
+          <input
+            type="text"
+            id="phoneInput"
+            value={currentPhone}
+            onChange={phoneChange}
+          />
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+        {<GeneralInfo info={info}/>}
+
+      </div>
+    )
   }
 }
 
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,27 +1,13 @@
 import React from 'react';
 
-class GeneralInfo extends React.Component {
-  constructor(props) {
-    super(props);
-
-
-    this.state = {
-      props
-    }
-
-  };
-
-
-  render() {
-
-    return (
-      <div id="generalInfo">
-        <div>Name: {this.props.info.name}</div>
-        <div>Email: {this.props.info.email}</div>
-        <div>Phone: {this.props.info.phone}</div>
-      </div>
-    );
-  };
-};
+const GeneralInfo = (props) => {
+  return (
+    <div id="generalInfo">
+        <div>Name: {props.info.name}</div>
+        <div>Email: {props.info.email}</div>
+        <div>Phone: {props.info.phone}</div>
+    </div>
+  )
+}
 
 export default GeneralInfo;
